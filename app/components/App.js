e6eb585981c845f68982d6d75ea0d7f3c0f3e26d
Stephanie Tassone <stephanietassone@Stephanies-MacBook-Pro.local>
2017-06-08 15:51:07 +0200

    First commit

---
+++ app/components/App.js
@@ -0,0 +1,34 @@
+var React = require('react');
+var Popular = require ('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Results = require('./Results');
+var Switch = ReactRouter.Switch;
+
+class App extends React.Component {
+    render () {
+        return (
+            <Router>
+                <div className='container'>
+                     <Nav />
+                     <Switch>
+                         <Route exact path='/' component={Home} />
+                         <Route path='/popular' component={Popular} />
+                         <Route exact path='/battle/results' component={Results} />
+                         <Route exact path='/battle' component={Battle} />
+                         <Route render={function () {
+                            return <p>Not Found :(</p>
+                            }}/>
+                     </Switch>
+                </div>
+            </Router>
+        )
+    }
+}
+
+module.exports = App ;
+
---
+++ app/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./styles.css');
+var App = require('./components/App.js');
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
